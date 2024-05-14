bbc02ee191346a0243bad024397179b8e9b8a067
Sameer Ahmad <155946235+Sameer-Ahmad-0@users.noreply.github.com>
2024-05-14 11:11:42 +0500

    Few changes

--- calculator/src/Component/Calculator.jsx
+++ calculator/src/Component/Calculator.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import './Calculator.css';
+import React, { useState } from "react";
+import "./Calculator.css";
 
 const Calculator = () => {
-  const [displayValue, setDisplayValue] = useState('');
+  const [displayValue, setDisplayValue] = useState("");
 
   const appendToDisplay = (value) => {
     setDisplayValue((prevValue) => prevValue + value);
   };
 
   const clearDisplay = () => {
-    setDisplayValue('');
+    setDisplayValue("");
   };
 
   const calculate = () => {
     try {
-      const sanitizedInput = displayValue.replace(/[^-()\d/*+.]/g, ''); 
-      const result = new Function('return ' + sanitizedInput)(); 
-      setDisplayValue(displayValue + ' = ' + result);
+      const sanitizedInput = displayValue.replace(/[^-()\d/*+.]/g, "");
+      const result = new Function("return " + sanitizedInput)();
+      setDisplayValue(result.toString());
     } catch (error) {
-      setDisplayValue('Error');
+      setDisplayValue("Error");
     }
   };
 
   const deleteLastChar = () => {
     setDisplayValue((prevValue) => {
       if (prevValue.length > 0) {
-        return prevValue.slice(0, -1); 
+        return prevValue.slice(0, -1);
       }
       return prevValue;
     });
@@ -33,27 +33,51 @@ const Calculator = () => {
 
   return (
     <>
-      <div id='calculator'>
-        <input id='display' value={displayValue} readOnly />
-        <div id='key'>
-          <button onClick={() => appendToDisplay('+ ')} className='operator-btn'>+</button>
-          <button onClick={() => appendToDisplay('7 ')}>7</button>
-          <button onClick={() => appendToDisplay('8 ')}>8</button>
-          <button onClick={() => appendToDisplay('9 ')}>9</button>
-          <button onClick={() => appendToDisplay('- ')} className='operator-btn'>-</button>
-          <button onClick={() => appendToDisplay('4')}>4</button>
-          <button onClick={() => appendToDisplay('5 ')}>5</button>
-          <button onClick={() => appendToDisplay('6')}>6</button>
-          <button onClick={() => appendToDisplay('* ')} className='operator-btn'>*</button>
-          <button onClick={() => appendToDisplay('1')}>1</button>
-          <button onClick={() => appendToDisplay('2 ')}>2</button>
-          <button onClick={() => appendToDisplay('3')}>3</button>
-          <button onClick={() => appendToDisplay('/ ')} className='operator-btn'>/</button>
-          <button onClick={() => appendToDisplay('0')}>0</button>
-          <button onClick={() => appendToDisplay('.')}>.</button>
+      <div id="calculator">
+        <input id="display" value={displayValue} readOnly />
+        <div id="key">
+          <button
+            onClick={() => appendToDisplay("+ ")}
+            className="operator-btn"
+          >
+            +
+          </button>
+          <button onClick={() => appendToDisplay("7 ")}>7</button>
+          <button onClick={() => appendToDisplay("8 ")}>8</button>
+          <button onClick={() => appendToDisplay("9 ")}>9</button>
+          <button
+            onClick={() => appendToDisplay("- ")}
+            className="operator-btn"
+          >
+            -
+          </button>
+          <button onClick={() => appendToDisplay("4")}>4</button>
+          <button onClick={() => appendToDisplay("5 ")}>5</button>
+          <button onClick={() => appendToDisplay("6")}>6</button>
+          <button
+            onClick={() => appendToDisplay("* ")}
+            className="operator-btn"
+          >
+            *
+          </button>
+          <button onClick={() => appendToDisplay("1")}>1</button>
+          <button onClick={() => appendToDisplay("2 ")}>2</button>
+          <button onClick={() => appendToDisplay("3")}>3</button>
+          <button
+            onClick={() => appendToDisplay("/ ")}
+            className="operator-btn"
+          >
+            /
+          </button>
+          <button onClick={() => appendToDisplay("0")}>0</button>
+          <button onClick={() => appendToDisplay(".")}>.</button>
           <button onClick={calculate}>=</button>
-          <button onClick={clearDisplay} className='operator-btn'>C</button>
-          <button onClick={deleteLastChar} className='operator-btn'>⌫</button>
+          <button onClick={clearDisplay} className="operator-btn">
+            C
+          </button>
+          <button onClick={deleteLastChar} className="operator-btn">
+            ⌫
+          </button>
         </div>
       </div>
     </>

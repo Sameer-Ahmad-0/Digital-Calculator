be2a7963ac4a2d07429655c00a148458ee26db0a
Sameer Ahmad <155946235+Sameer-Ahmad-0@users.noreply.github.com>
2024-05-14 17:02:45 +0500

    Little changes

--- calculator/src/Component/Calculator.jsx
+++ calculator/src/Component/Calculator.jsx
@@ -31,16 +31,16 @@ const Calculator = () => {
     });
   };
 
-  return (
+  return (/////
     <>
       <div id="calculator">
-        <input id="display" value={displayValue} readOnly />
+        <input id="display" value={displayValue} readOnly  placeholder="Q❤️H"/>
         <div id="key">
           <button
             onClick={() => appendToDisplay("+ ")}
             className="operator-btn"
           >
-            +
+            ➕
           </button>
           <button onClick={() => appendToDisplay("7 ")}>7</button>
           <button onClick={() => appendToDisplay("8 ")}>8</button>
@@ -49,7 +49,7 @@ const Calculator = () => {
             onClick={() => appendToDisplay("- ")}
             className="operator-btn"
           >
-            -
+            ➖
           </button>
           <button onClick={() => appendToDisplay("4")}>4</button>
           <button onClick={() => appendToDisplay("5 ")}>5</button>
@@ -58,7 +58,7 @@ const Calculator = () => {
             onClick={() => appendToDisplay("* ")}
             className="operator-btn"
           >
-            *
+            ✖️
           </button>
           <button onClick={() => appendToDisplay("1")}>1</button>
           <button onClick={() => appendToDisplay("2 ")}>2</button>
@@ -67,16 +67,16 @@ const Calculator = () => {
             onClick={() => appendToDisplay("/ ")}
             className="operator-btn"
           >
-            /
+           ➗
           </button>
           <button onClick={() => appendToDisplay("0")}>0</button>
           <button onClick={() => appendToDisplay(".")}>.</button>
-          <button onClick={calculate}>=</button>
+          <button onClick={calculate}>〓</button>
           <button onClick={clearDisplay} className="operator-btn">
-            C
+          C
           </button>
           <button onClick={deleteLastChar} className="operator-btn">
-            ⌫
+          ❌
           </button>
         </div>
       </div>
